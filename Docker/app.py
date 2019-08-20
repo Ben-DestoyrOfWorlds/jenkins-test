06f327b0d66b4ad07e1c6888a75bf5e949d61ec2
Ben Leavitt <ben.leavitt@cybera.ca>
2019-08-20 16:15:53 +0000

    Updating a few things

--- Docker/app.py
+++ Docker/app.py
@@ -1,29 +1,23 @@
 from flask import Flask
-from redis import Redis, RedisError
 import os
 import socket
-
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'hi'
+
 @app.route('/hello/')
 def hello_world():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+           "<b>Visits:</b> todo"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
     return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    app.run(host='0.0.0.0', port=80)     # open for everyone
